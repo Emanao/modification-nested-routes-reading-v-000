2454afa01071f46ea02769233e0e9d0e7ce6959b
github username <github email address>
2020-04-21 22:20:38 -0400

    Handle errors in URLs and expand strong param with author_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,12 @@ class PostsController < ApplicationController
 
   def index
     if params[:author_id]
-      @posts = Author.find(params[:author_id]).posts
+        author = Author.find_by(id: params[:author_id])
+        if author.nil?
+          redirect_to authors_path, alert: "Author not found" 
+        else 
+          @posts=author.posts
+        end  
     else
       @posts = Post.all
     end
@@ -10,9 +15,15 @@ class PostsController < ApplicationController
 
   def show
     if params[:author_id]
-      @post = Author.find(params[:author_id]).posts.find(params[:id])
+      author = Author.find_by(id:params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found" 
+      else 
+        @post=author.posts.find_by(id:params[:id])
+        redirect_to author_posts_path, alert: "Post not found" if @post.nil?
+      end
     else
-      @post = Post.find(params[:id])
+      @post = Post.find_by(id: params[:id])
     end
   end
 
@@ -53,6 +64,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
